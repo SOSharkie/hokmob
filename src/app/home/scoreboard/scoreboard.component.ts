fb7e66134d46a0048cfc79607962077b980bbd45
Sean Odnert <sosharkie@gmail.com>
2023-04-07 12:41:01 -0700

    Fixed css for scorecards

--- src/app/home/scoreboard/scoreboard.component.ts
+++ src/app/home/scoreboard/scoreboard.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {GameModel} from "../../shared/models/game.model";
 
 @Component({
   selector: 'app-scores',
   templateUrl: './scoreboard.component.html',
-  styleUrls: ['./scoreboard.component.scss']
+  styleUrls: ['./scoreboard.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ScoreboardComponent {
 
   public currentDayGames: GameModel[] = [
     {homeTeam: "Sharks", awayTeam: "Flyers", gameTime: {hours: 4, minutes: 30}},
-    {homeTeam: "Kings", awayTeam: "Rangers", gameTime: {hours: 6, minutes: 0}},
+    {homeTeam: "Kings", awayTeam: "Rangers", gameTime: {hours: 6, minutes: 30}},
     {homeTeam: "Penguins", awayTeam: "Avalanche", gameTime: {hours: 7, minutes: 30}},
   ];
 }
