ebe7be35148cc96cf9323aac4c395e85dabc0a1b
Sean Odnert <sosharkie@gmail.com>
2023-04-07 14:42:56 -0700

    Added datepicker dropwdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { HomeComponent } from './home/home.component';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { HeaderComponent } from './header/header.component';
 import { SearchInputComponent } from './shared/components/search-input/search-input.component';
 import { NavigationMenuComponent } from './home/navigation-menu/navigation-menu.component';
 import { ScoreboardComponent } from './home/scoreboard/scoreboard.component';
 import { ScorecardComponent } from './shared/components/scorecard/scorecard.component';
+import { MatNativeDateModule } from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -30,7 +32,9 @@ import { ScorecardComponent } from './shared/components/scorecard/scorecard.comp
     BrowserAnimationsModule,
     MatToolbarModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatNativeDateModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/scoreboard/scoreboard.component.ts
+++ src/app/home/scoreboard/scoreboard.component.ts
@@ -1,5 +1,6 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
 import {GameModel} from "../../shared/models/game.model";
+import {MatDatepicker} from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-scores',
@@ -9,9 +10,16 @@ import {GameModel} from "../../shared/models/game.model";
 })
 export class ScoreboardComponent {
 
+  @ViewChild('datePicker')
+  public datePicker: MatDatepicker<any>;
+
   public currentDayGames: GameModel[] = [
     {homeTeam: "Sharks", awayTeam: "Flyers", gameTime: {hours: 4, minutes: 30}},
     {homeTeam: "Kings", awayTeam: "Rangers", gameTime: {hours: 6, minutes: 30}},
     {homeTeam: "Penguins", awayTeam: "Avalanche", gameTime: {hours: 7, minutes: 30}},
   ];
+
+  public onClickDate(): void {
+    this.datePicker.open();
+  }
 }
