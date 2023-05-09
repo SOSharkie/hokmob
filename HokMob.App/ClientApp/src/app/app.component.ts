b096070d91060524da20104f2cefe158cabc5df0
Mark Hiserodt <markhiserodt@cypherfusion.com>
2023-05-09 15:27:05 -0400

    Dynamic styling with ngClass: Mobile menu selected

--- HokMob.App/ClientApp/src/app/app.component.ts
+++ HokMob.App/ClientApp/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {NavigationEnd, Router} from "@angular/router";
 import {Subject, takeUntil} from "rxjs";
-import {RouterExtensionService} from "@shared/services/router-extension.service";
 
 @Component({
   selector: 'app-root',
@@ -13,17 +12,16 @@ export class AppComponent implements OnInit, OnDestroy {
   clientHeight: number;
   title = 'hokmob';
   ngUnsubscribe = new Subject<void>();
+  menuUrl: string = '';
 
-  constructor(private route: ActivatedRoute,
-              private router: Router,
-              private routerExtensionService: RouterExtensionService) {
+  constructor(private router: Router) {
     this.clientHeight = window.innerHeight;
   }
 
   public ngOnInit(): void {
     this.router.events.pipe(takeUntil(this.ngUnsubscribe)).subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.onMenuItemClick(event.url);
+        this.menuUrl = event.url;
       }
       window.scrollTo(0, 0);
     });
@@ -32,27 +30,5 @@ export class AppComponent implements OnInit, OnDestroy {
   public ngOnDestroy(): void {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
-  }
-
-  public onMenuItemClick(item: string): void {
-    switch (item) {
-      case '/games':
-        document.getElementById("games-item").classList.add("selected");
-        document.getElementById("playoffs-item").classList.remove("selected");
-        document.getElementById("stats-item").classList.remove("selected");
-
-        break;
-      case '/playoffs':
-        document.getElementById("games-item").classList.remove("selected");
-        document.getElementById("playoffs-item").classList.add("selected");
-        document.getElementById("stats-item").classList.remove("selected");
-
-        break;
-      case '/stats':
-        document.getElementById("games-item").classList.remove("selected");
-        document.getElementById("playoffs-item").classList.remove("selected");
-        document.getElementById("stats-item").classList.add("selected");
-        break;
-    }
   }
 }
